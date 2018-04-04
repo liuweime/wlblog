4c7c714527ae44f0d2b62b60a146199957c3ad0f
liuwei <liuweiprog@foxmail.com>
2018-04-04 16:54:18 +0800

    '自定义异常处理方式'

---
+++ app/Exceptions/LogicException.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘威
+ * Date: 2018/4/4
+ * Time: 16:18
+ */
+
+namespace App\Exceptions;
+
+
+use Exception;
+use Illuminate\Http\Request;
+
+class LogicException extends Exception
+{
+    protected $name = 'logic';
+
+    protected $code = '00';
+
+    public function __construct(array $message)
+    {
+        // 读取配置
+        $exceptionName = $this->name;
+        $exceptionConfig = config('exception.' . $exceptionName);
+
+        // 读取错误信息
+        list($info, $format) = $message;
+        $info = $exceptionConfig[$info];
+        // 格式化
+        $msg = sprintf($info[1], $format);
+
+        parent::__construct($msg, $this->code . $info[0]);
+    }
+
+    public function report()
+    {
+
+    }
+
+    public function render(Request $request)
+    {
+        $message = $this->getMessage();
+        $code = $this->getCode();
+
+        if ($request->ajax()) {
+            return response()->json([
+                'code' => $code,
+                'msg' => $message,
+                'info' => null
+            ]);
+        }
+    }
+}
---
+++ config/exception.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'article' => [
+        'ARTICLE_TITLE_EXISTS' => ['01', '文章(%s)已存在'],
+        'CREATE_ARTICLE_ERROR' => ['02', '文章(%s)建立失败']
+    ]
+];
